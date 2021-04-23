853e661456cba6383a64d4b3dafd9e7d2e014e8d
Chain Yu <chain.yu@sap.com>
2021-04-23 17:30:48 +0800

    P1

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,55 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, {useContext, useState} from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+interface IAppState {
+    user: {
+        name: string;
+        age: number;
+    };
 }
 
-export default App
+interface ContextType {
+    appState: IAppState;
+    setAppState: (state: IAppState) => void;
+}
+
+const appContext = React.createContext<ContextType | null>(null);
+
+export const App: React.FC = () => {
+    const [ appState, setAppState ] = useState<IAppState>({
+        user: { name: 'chain', age: 33 },
+    });
+    //const contextValue = { appState, setAppState };
+    return (
+        <appContext.Provider value={{ appState, setAppState }} >
+          <大儿子/>
+          <二儿子/>
+          <幺儿子/>
+        </appContext.Provider>
+      );
+};
+
+const 大儿子 = () => <section>大儿子<User/></section>;
+const 二儿子 = () => <section>二儿子<UserModifier/></section>;
+const 幺儿子 = () => <section>幺儿子</section>;
+
+const User = () => {
+    const contextValue = useContext(appContext) as ContextType;
+    return (
+        <div> User:{ contextValue.appState.user.name } </div>
+    );
+};
+
+const UserModifier = () => {
+    const contextValue = useContext(appContext) as ContextType;
+    const onChange = (e: any) => {
+        contextValue.appState.user.name = e.target.value;
+        contextValue.setAppState({...contextValue.appState});
+  };
+  return (
+    <div>
+      <input value={ contextValue.appState.user.name }
+             onChange={onChange}/>
+    </div>);
+};
+
